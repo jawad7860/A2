4dee055a62dfa0e622ac16483ca780e57d94c3e5
jawad7860 <afzaljawad297@gmail.com>
2023-03-21 10:30:00 +0500

    u

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
+"""
+This module implements a Flask application for predicting stock prices.
+"""
+
 from flask import Flask, render_template, request
 import joblib
 
 app = Flask(__name__)
 
 
-# Homepage route
 @app.route('/')
 def home():
-
+    """
+    Render the homepage template.
+    """
     return render_template('index.html')
 
-# Prediction route
-@app.route('/predict', methods=['POST'])
 
+@app.route('/predict', methods=['POST'])
 def predict():
-    
+    """
+    Predict the stock price based on the form data.
+    """
     loaded_model = joblib.load('model1.pkl')
 
     open_price = float(request.form['open'])
     high_price = float(request.form['high'])
     low_price = float(request.form['low'])
     volume = float(request.form['volume'])
-    
-  
-    
 
-    
     # Make prediction
     new_features = [[open_price, high_price, low_price, volume]]
     predicted_price = loaded_model.predict(new_features)
-    
-    return render_template('index.html', prediction='Predicted Price: {}'.format(predicted_price[0]))
+
+    return render_template('index.html', prediction=f'Predicted Price: {predicted_price[0]}')
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
-    # Get form data
