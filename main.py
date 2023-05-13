b661fafd1a577f8c68c8425f459fb5bec8f32793
jawad7860 <59655762+jawad7860@users.noreply.github.com>
2023-05-13 15:57:20 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+import joblib
+from sklearn.linear_model import LinearRegression
+app = Flask(__name__)
+
+
+# Homepage route
+@app.route('/')
+def home():
+
+    return render_template('index.html')
+
+# Prediction route
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    
+    loaded_model = joblib.load('model1.pkl')
+
+    open_price = float(request.form['open'])
+    high_price = float(request.form['high'])
+    low_price = float(request.form['low'])
+    volume = float(request.form['volume'])
+    
+  
+    
+
+    
+    # Make prediction
+    new_features = [[open_price, high_price, low_price, volume]]
+    predicted_price = loaded_model.predict(new_features)
+    
+    return render_template('index.html', prediction='Predicted Price: {}'.format(predicted_price[0]))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    # Get form data
